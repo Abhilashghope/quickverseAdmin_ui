76cd90bc4d1e832e5ed9bb1f6be363068e028c86
Abhilashghope <abhilashghope111@gmail.com>
2025-05-05 21:37:34 +0530

    added login screen wih navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import AppStack from './src/components/route/AppStack';
 import {NavigationContainer} from '@react-navigation/native';
+import {Router} from './src/components/route/Route';
+import {AuthProvider} from './src/contexts/Login/AuthProvider';
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      {' '}
-      <AppStack />
-    </NavigationContainer>
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   );
 }
 
---
+++ src/components/route/AuthStack.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import LoginScreen from '../../screens/Login/loginScreen';
+
+export const AuthStack = () => {
+  return <LoginScreen />;
+};
